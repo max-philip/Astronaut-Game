777e815ed8b650b32f64ff051dc9d57e7a637d9a
Max Philip <mphilip1@student.unimelb.edu.au>
2018-10-08 15:52:06 +1100

    change shaders on enemies, day night cycle

---
+++ Astronaut-Game/Assets/Scripts/sunOrbit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sunOrbit : MonoBehaviour
+{
+    public float CycleSpeed = 15.0f;
+
+    void Update()
+    {
+        Vector3 origin = new Vector3(0.0f, 0.0f, 0.0f);
+
+        // Object rotates around in an orbit. LookAt is used since this will be
+        // applied to a direcitonal light
+        transform.LookAt(origin);
+        transform.RotateAround(origin, Vector3.right, Time.deltaTime * CycleSpeed);
+    }
+
+    public void changeOrbit(float NewCycle)
+    {
+        CycleSpeed = NewCycle;
+    }
+}
